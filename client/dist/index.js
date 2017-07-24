51f26f4d6874321b3e2b27d8793d73e7160c0161
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-07-23 18:05:42 -0700

    (feat) Add event listener to make get request on inputs to form

--- client/dist/index.js
+++ client/dist/index.js
@@ -1,12 +1,18 @@
+'use strict';
+
 window.onload = () => {
   let input = document.querySelector('#name-input');
   let namesList = document.getElementById('names-list');
 
   input.addEventListener('input', (event) => {
-    console.log(input.value);
-    $.get('/getAllNames', (requestedNames) => {
+    // console.log(input.value);
 
-    });
+    if (input.value.length > 0) {
+      $.get(`/getNames?input=${input.value}`, (requestedNames) => {
+        console.log(requestedNames);
+      });
+    }
+
     event.preventDefault();
   });
 };
