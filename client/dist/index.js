11e10aaa7446055f1d1e42c483dedf4b154a17df
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-07-23 18:42:36 -0700

    (feat) Add event listener to handle submit

--- client/dist/index.js
+++ client/dist/index.js
@@ -3,16 +3,29 @@
 window.onload = () => {
   let input = document.querySelector('#name-input');
   let namesList = document.getElementById('names-list');
+  let submit = document.querySelector('#name-submit');
 
   input.addEventListener('input', (event) => {
     let trimmedInputValue = input.value.trim();
 
+    if (trimmedInputValue === '') namesList.innerHTML = '';
+
     if (trimmedInputValue.length > 0) {
       $.get(`/getNames?input=${trimmedInputValue}`, (requestedNames) => {
-        // console.log(requestedNames);
+        namesList.innerHTML = '';
+
+        requestedNames.forEach((name) => {
+          let listItem = document.createElement('li');
+         
+          listItem.innerHTML = name;
+          namesList.appendChild(listItem);
+        });
       });
     }
+  });
 
+  submit.addEventListener('submit', (event) => {
+    
     event.preventDefault();
   });
 };
